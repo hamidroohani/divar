b23d2883e8f43a4f8c23f9e3312acfe3f75926f2
Hamid <Hamid.dev@yahoo.com>
2018-10-12 18:16:32 +0330

    sorted correctly items in categories and my_items

--- resources/views/pages/categories.blade.php
+++ resources/views/pages/categories.blade.php
@@ -31,36 +31,30 @@
                     <div class="panel-body">
                         @if(isset($items))
                             <div class="row">
-                                <?php
-                                $i = 0;
-                                ?>
                                 @if(count($items) == 0)
                                         <h4><p class="centeralign" style="color: red">!!هنوز هیچ آگهی در این زمینه ارسال نشده</p></h4>
                                 @endif
-                                @foreach($items as $item)
+                                    @for($i = (count($items)) - 1; $i >= 0; $i--)
                                     <div class="col-lg-6">
                                         <div class="panel panel-default">
                                             <div class="panel-heading centeralign">
-                                                <a href="/information/{{ $item->id }}">
-                                                    <h4>{{$item->title}}</h4>
+                                                <a href="/information/{{ $items[$i]->id }}">
+                                                    <h4>{{$items[$i]->title}}</h4>
                                                 </a>
                                             </div>
                                             <div class="panel-body">
-                                                <img src="{{ asset('storage/'. $item->img_id) }}" class="img-responsive"
+                                                <img src="{{ asset('storage/'. $items[$i]->img_id) }}" class="img-responsive"
                                                      style="width:100%;height: 40%" alt="Image">
                                             </div>
                                             <div class="panel-footer">
-                                                <span style="float: right"><b>گروه :</b>{{$categories[$item->category_id-1]->title}}</span>
-                                                <span style="margin: 0 40px"><b>قیمت :</b>{{$item->price}}</span>
-                                                <span style="float: left"><b>محله :</b>{{$item->position}}</span>
+                                                <span style="float: right"><b>گروه :</b>{{$categories[$items[$i]->category_id-1]->title}}</span>
+                                                <span style="margin: 0 40px"><b>قیمت :</b>{{$items[$i]->price}}</span>
+                                                <span style="float: left"><b>محله :</b>{{$items[$i]->position}}</span>
                                             </div>
                                             <div class="clearfix"></div>
                                         </div>
                                     </div>
-                                    <?php
-                                    $i++;
-                                    ?>
-                                @endforeach
+                                @endfor
                             </div>
                         @else
                             <h4><p class="centeralign">لطفا یک دسته بندی را انتخاب کنید</p></h4>
--- resources/views/pages/my_items.blade.php
+++ resources/views/pages/my_items.blade.php
@@ -8,31 +8,28 @@
     <div class="container">
         <div class="row">
             <?php
-            $i = 0;
+                $items = Auth()->user()->items;
             ?>
-            @foreach(Auth()->user()->items as $item)
+            @for($i = (count($items)) - 1; $i >= 0; $i--)
                 <div class="col-sm-4">
                     <div class="panel panel-default">
                         <div class="panel-heading centeralign">
-                            <a href="/information/{{ $item->id }}">
-                                <h4>{{$item->title}}</h4>
+                            <a href="/information/{{ $items[$i]->id }}">
+                                <h4>{{$items[$i]->title}}</h4>
                             </a>
                         </div>
                         <div class="panel-body">
-                            <img src="{{ asset("storage/".$item->img_id) }}" class="img-responsive" style="width:100%;height: 50%" alt="Image">
+                            <img src="{{ asset("storage/".$items[$i]->img_id) }}" class="img-responsive" style="width:100%;height: 50%" alt="Image">
                         </div>
                         <div class="panel-footer">
-                            <span style="float: right"><b>گروه :</b>{{$categories[$item->category_id-1]->title}}</span>
-                            <span style="margin: 0 40px"><b>قیمت :</b>{{$item->price}}</span>
-                            <span style="float: left"><b>محله :</b>{{$item->position}}</span>
+                            <span style="float: right"><b>گروه :</b>{{$categories[$items[$i]->category_id-1]->title}}</span>
+                            <span style="margin: 0 40px"><b>قیمت :</b>{{$items[$i]->price}}</span>
+                            <span style="float: left"><b>محله :</b>{{$items[$i]->position}}</span>
                         </div>
                         <div class="clearfix"></div>
                     </div>
                 </div>
-                <?php
-                $i++;
-                ?>
-            @endforeach
+            @endfor
         </div>
     </div>
 @endsection
